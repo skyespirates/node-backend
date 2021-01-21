145914c1b09ebe9396f9c490ca7a0783bac5fb03
Skyes Crawford <akashiseijurou414@gmail.com>
2021-01-21 23:48:30 +0700

    CRUD operation on mongoose

--- MongooseBasic/index.js
+++ MongooseBasic/index.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017/movieApp", {
+  .connect("mongodb://localhost:27017/school", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then(() => {
     console.log("CONNECTED TO MONGOO DB");
@@ -12,37 +13,87 @@ mongoose
     console.log(err);
   });
 
-//  CREATE MOVIE SCHEMA
-const movieSchema = new mongoose.Schema({
-  name: String,
-  year: Number,
-  score: Number,
-});
-//  CREATE MOVIE MODEL
-const Movie = mongoose.model("Movie", movieSchema);
-//  CREATE MOVIE INSTANCE
-// const dragon = new Movie({name: 'dragon', year: 2012, score: 7.5})
-//  SAVE MOVIE INSTANCE TO DATABASE
-//  dragon.save()
-
-//  CREATE STUDENT SCHEMA
+//CREATE STUDENT SCHEMA
 const studentSchema = new mongoose.Schema({
   name: String,
+  sex: String,
   age: Number,
-  isActive: Boolean,
+  status: Boolean,
 });
-//  CREATE STUDENT MODEL
+//CREATE STUDENT MODEL
 const Student = new mongoose.model("Student", studentSchema);
-//  CREATE STUDENT INSTANCE
-// const skyes = new Student({ name: "skyes crawford", age: 21, isActive: true });
-//  SAVE STUDENT INSTANCE TO DATABASE
-// skyes.save()
+//CREATE STUDENT INSTANCE
+const skyes = new Student({
+  name: "skyes crawford",
+  sex: "male",
+  age: 18,
+  status: true,
+});
+//INSERTING ONE DOCUMENT
+// skyes.save();
 
-//  CREATE TODO SCHEMA
-const todoSchema = new mongoose.Schema({ todo: String, isComplete: Boolean });
-//  CREATE TODO MODEL
-const Todo = new mongoose.model("Todo", todoSchema);
-//  CREATE TODO INSTANCE
-// const running = new Todo({ todo: "running", isComplete: false });
-//  SAVE TODO INSTANCE TO DATABASE
-// running.save();
+//INSERTING MANY DOCUMENT
+Student.insertMany([
+  {
+    name: "uzumaki naruto",
+    sex: "male",
+    age: 22,
+    status: false,
+  },
+  {
+    name: "uchiha sasuke",
+    sex: "male",
+    age: 23,
+    status: false,
+  },
+  {
+    name: "mikasa ackerman",
+    sex: "female",
+    age: 21,
+    status: true,
+  },
+]).then((data) => {
+  console.log("MANY DATA HAS INSERTED");
+  console.log(data);
+});
+
+//FINDING DOCUMENT
+
+//FIND EVERYTHING IN COLLECTION
+Student.find({}).then((data) => console.log(data));
+//FIND SPECIFIC DOCUMENT
+Student.find({ name: "mikasa ackerman" }).then((data) => console.log(data));
+//FIND WITH CONSTRAINT
+Student.find({ age: { $gt: 20 } }).then((data) => console.log(data));
+//FIND DOCUMENT BY ID
+Student.findById("6009a329a13f61191ca95ece").then((data) => console.log(data));
+
+//UPDATING DOCUMENT
+
+//UPDATE ONE DOCUMENT
+Student.updateOne(
+  { name: "skyes crawford" },
+  { name: "hashirama senju" }
+).then((data) => console.log(data));
+//UPDATE MANY DOCUMENTS
+Student.updateMany(
+  { name: { $in: ["uzumaki naruto", "uchiha sasuke"] } },
+  { age: 25 }
+).then((res) => console.log(res));
+//FIND ONE AND UPDATA
+Student.findOneAndUpdate(
+  { name: "uchiha itachi" },
+  { name: "uchiha madara" },
+  { new: true }
+).then((res) => console.log(res));
+
+//DELETING DOCUMENT
+
+//DELETING ONE DOCUMENT
+Student.remove({ name: "kagami taiga" }).then((msg) => console.log(msg));
+//DELETING MANY DOCUMENTS
+Student.deleteMany({ age: { $lte: 20 } }).then((msg) => console.log(msg));
+//DELETING ONE DOCUMENT AND GET RETURNING DATA
+Student.findOneAndDelete({ name: "uzumaki naruto" }).then((msg) =>
+  console.log(msg)
+);
